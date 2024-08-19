e240753e2f265d3dc81c2ebc5f1fa2cf8e5fad0a
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2024-08-19 03:05:53 +0300

    Add files via upload

---
+++ AI_Tasks/Task2/main.py
@@ -0,0 +1,12 @@
+'''
+Problem7
+Create a module containing functions for
+mathematical operations (addition, subtraction, multiplication, division)
+'''
+import mod;
+
+print("Addition:", mod.addition(10, 3))
+print("Subtraction:", mod.subtraction(10, 2))
+print("Multiplication:", mod.multiplication(2, 3))
+print("Division:", mod.division(8, 0))
+
